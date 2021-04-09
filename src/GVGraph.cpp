368e3479f8858e9a8f6dc68cbdd59ea3b2039482
phongsun <phsun@smu.edu>
2021-04-08 23:05:25 -0600

    generated a Directed Acylic Graph using Breadth First Search and calculated shortest path for each node

--- src/GVGraph.cpp
+++ src/GVGraph.cpp
@@ -35,24 +35,94 @@ GVGraph::GVGraph(vector<Edge> edgeList)
     }
 }
 
-Graph GVGraph::computeDAG()
+Graph GVGraph::computeBetweeness()
 {
+    typedef graph_traits<DAG>::out_edge_iterator out_edge_it;
+    typedef graph_traits<DAG>::in_edge_iterator in_edge_it;
 
-    struct Visitor : public boost::default_bfs_visitor {
-        void discover_vertex(Vertex v, const Graph &) const {
-            std::cout << "Current: " << v << std::endl;
+    Vertex start = this->indexes["A"];
+    // BFS DAG
+    DAG dag = DAG(num_vertices(g));
+    // map is used to track vertices at each level
+    map<int, set<Vertex>> levelMap = map<int, set<Vertex>>();
+    levelMap[0] = set<Vertex>({start});
+    Visitor visitor(dag, start, levelMap);
+
+    // 1. BFS from A and have the vistor to construct a DAG
+    boost::breadth_first_search(g, start, boost::visitor(visitor));
+
+    // 2. top-down: calculate a shortest path count for each vertex
+    for (int i = 1; i < levelMap.size(); i++) {
+        for(auto node: levelMap[i]) {
+            out_edge_it ei, ei_end;
+            for (boost::tie(ei, ei_end) = out_edges(node, dag); ei != ei_end; ++ei){
+                Vertex t = target(*ei, dag);
+                // skip edges in the DAG if they are on the same level
+                if (!levelMap[i].count(t)) {
+                    int shortestPathCount = get(boost::vertex_rank_t(), dag, t) + get(boost::vertex_rank_t(), dag, node);
+                    boost::put(vertex_rank_t(), dag, t, shortestPathCount);
+                }
+            }
         }
-    };
-    /*
-Vertex start = 0;
-        Visitor visitor;
+    }
 
-         boost::breadth_first_search(g, start, boost::visitor(visitor));
+    // 3. bottom-up: calculate betweeness for each edge
+    for (int i = levelMap.size() - 1; i > 0; i--) {
+        for(auto node: levelMap[i]) {
+            // each node get a credit of 1 + sum of out edges weight
+            float nodeCredit = 1;
+            out_edge_it ei, ei_end;
+            for (boost::tie(ei, ei_end) = out_edges(node, dag); ei != ei_end; ++ei){
+                float wt = get(boost::edge_weight_t(), dag, *ei);
+                nodeCredit += wt;
+            }
 
-        
-        
-        
-         return g;
-         */
-    return NULL;
+            /*in_edge_it in_ei, in_ei_end;
+            for (boost::tie(in_ei, in_ei_end = in_edges(node, dag); in_ei != in_ei_end; ++in_ei){
+                float wt = get(boost::edge_weight_t(), dag, *ei);
+                nodeCredit += wt;
+            }*/
+
+            // sum shortest_path_count of all in_edge nodes (sum_s_p_c),
+
+            // in_edge weight = s_p_c/sum_s_p_c + credit
+            /*graph_traits<DAG>::out_edge_iterator i_ei, i_ei_end;
+            for (boost::tie(i_ei, i_ei_end) = out_edges(node, dag); i_ei != i_ei_end; ++i_ei){
+                Vertex t = target(*i_ei, dag);
+                // skip edges in the DAG if they are on the same level
+                if (!levelMap[i].count(t)) {
+                    int shortestPathCount = get(boost::vertex_rank_t(), dag, t) + get(boost::vertex_rank_t(), dag, node);
+                    boost::put(vertex_rank_t(), dag, t, shortestPathCount);
+                }
+            }*/
+        }
+    }
+
+    // calculate shortest path count for each node
+
+
+    std::cout << "edges(dag) = ";
+    graph_traits<DAG>::edge_iterator ei, ei_end;
+    for (boost::tie(ei, ei_end) = edges(dag); ei != ei_end; ++ei)
+    {
+        std::cout << "("
+                  << lookup[source(*ei, dag)]
+                  << "," << lookup[target(*ei, dag)]
+                  << ") ";
+    }
+    std::cout << std::endl;
+    for (auto const &pair : levelMap)
+    {
+        cout << "level - " << pair.first << endl;
+
+        auto vList = pair.second;
+        for (auto x : vList)
+        {
+            int p = boost::get(boost::vertex_rank_t(), dag, x);
+            cout << lookup[x] << "(" << p << ") ";
+        }
+        std::cout << std::endl;
+    }
+
+    return g;
 }
--- src/GVGraph.h
+++ src/GVGraph.h
@@ -9,6 +9,7 @@
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/dijkstra_shortest_paths.hpp>
+#include <boost/graph/graphviz.hpp>
 #include <iostream>
 #include <iterator>
 #include <algorithm>
@@ -19,30 +20,89 @@
 using namespace std;
 using namespace boost;
 
-//define a custom type of Edge for pair<string,string>
+//define a custom type of Edge for pair
 typedef std::pair<std::string, std::string> Edge;
-//define a custom type of VertexProperty
-typedef property<vertex_name_t, std::string> VertexProperty;
-//define a custom type of Graph for undirected adjacency_list
-typedef adjacency_list<vecS, vecS, undirectedS, VertexProperty> Graph;
-//using Vertex for vertex_descriptor which is a number
+typedef property<vertex_name_t, std::string> VertexName;
+typedef property<edge_weight_t, float> EdgeWeight;
+typedef adjacency_list<vecS, vecS, undirectedS, VertexName, EdgeWeight> Graph;
+typedef property<vertex_rank_t, int> VertexShortestPathCount;
+typedef adjacency_list<vecS, vecS, directedS, VertexShortestPathCount, EdgeWeight> DAG;
 using Vertex = Graph::vertex_descriptor;
+
 /**
  * @brief The GVGraph class implements Girvan-Newman algorithm using Boost Graphic Library
  */
-class GVGraph
-{
+class GVGraph {
+    /**
+     * @brief The Vistor struct creates a BFS DAG
+     */
+    struct Visitor : public boost::default_bfs_visitor {
+    public:
+        DAG &dag;
+        int rootV;
+        map<int, set<Vertex>> &levelMap;
+
+        Visitor(DAG &iDag, int rootNode, map<int, set<Vertex>> &iMap) : dag(iDag), rootV(rootNode), levelMap(iMap) {}
+
+        void black_target(Graph::edge_descriptor e, const Graph &g) {
+            //string source = get(boost::vertex_name_t(), g, boost::source(e, g));
+            //string target = get(boost::vertex_name_t(), g, boost::target(e, g));
+
+            Vertex s = boost::target(e, g);
+            Vertex t = boost::source(e, g);
+            add_edge(s, t, 0.0, this->dag);
+            int level = 0;
+            if (s == rootV) {
+                level = 1;
+                // for target of root, give 1 for shortest path count to the vertex
+                boost::put(vertex_rank_t(), this->dag, t, 1);
+            } else {
+                // initialize for shortest path count to the vertex
+                boost::put(vertex_rank_t(), this->dag, t, 0);
+                for (auto const &pair : levelMap) {
+                    level = pair.first;
+                    auto vList = pair.second;
+                    if (std::find_if(vList.begin(), vList.end(), [s](const int v) { return v == s; }) != vList.end()) {
+                        ++level;
+                        break;
+                    }
+                }
+            }
+            if (levelMap.find(level) == levelMap.end()) {
+                levelMap[level] = set<Vertex>();
+            }
+            int deleteLevel = -1;
+            for (auto const &pair : levelMap) {
+                // calculate
+                auto vList = pair.second;
+                // if t is in a existing level
+                if (std::find_if(vList.begin(), vList.end(), [t](const int v) { return v == t; }) != vList.end()) {
+                    if (pair.first > level) { // t already exist in high level set, we need to delete it
+                        deleteLevel = pair.first;
+                        break;
+                    } else {
+                        return;
+                    }
+                }
+            }
+            if (deleteLevel > 0) {
+                levelMap[deleteLevel].erase(t);
+            }
+            levelMap[level].insert(t);
+        }
+    };
+
 private:
     Graph g; //Represents the graph of the input connection data
     std::map<std::string, Vertex> indexes; //key: text name of the vertex, value: number index of the vertex
     std::map<Vertex, std::string> lookup; //key: number index of the vertex, value: text name of the vertex
 
-    Graph computeDAG();
-
 public:
     GVGraph(vector<Edge> edgeList); //construct a graph with a list of social network connection edges
     ~GVGraph() {}
+
     Graph getGraph() { return g; } //return the original graph
+    Graph computeBetweeness();
 };
 
 
